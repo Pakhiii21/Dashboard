e672019b3671b3d333d9d0db60c392300a0fcf1d
realtime-excel-viewer <pkapoor_btech23@thapar.edu>
2025-07-20 13:59:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+ import streamlit as st 
 import pandas as pd
 import altair as alt
 
@@ -100,7 +100,7 @@ if uploaded_file:
                 st.success("✅ All samples meet standard parameters.")
                 st.dataframe(filtered, use_container_width=True)
 
-        except Exception as e:
+            except Exception as e:
 
 
 
